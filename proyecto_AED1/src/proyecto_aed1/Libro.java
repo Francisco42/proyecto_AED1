b56a2c855add1ce5336f535bd233089de2992ce5
Unknown <franpiria42@gmail.com>
2018-04-14 13:27:24 -0300

    clase libro llena

--- proyecto_AED1/src/proyecto_aed1/Libro.java
+++ proyecto_AED1/src/proyecto_aed1/Libro.java
@@ -10,5 +10,47 @@ package proyecto_aed1;
  * @author Francisco
  */
 public class Libro {
-    
+    private final String nombre;
+    private final String autor;
+    private final long isbn;
+    private final short año;
+    private final String genero;
+    private Libro siguiente;
+
+    public Libro(String nombre, String autor, long isbn, short año, String genero) {
+        this.nombre = nombre;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.año = año;
+        this.genero = genero;
+        this.siguiente = null;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public long getISBN() {
+        return isbn;
+    }
+
+    public short getAño() {
+        return año;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public Libro getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Libro siguiente) {
+        this.siguiente = siguiente;
+    } 
 }
