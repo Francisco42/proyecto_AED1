31be0a3241832e1ac798a4f774ff5f79ea7bf83b
Unknown <franpiria42@gmail.com>
2018-05-13 13:11:30 -0300

    termina equals en lista

--- proyecto_AED1/src/proyecto_aed1/Proyecto_AED1.java
+++ proyecto_AED1/src/proyecto_aed1/Proyecto_AED1.java
@@ -16,6 +16,43 @@ public class Proyecto_AED1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here  
+        Lista<Autor> lista1 = new Lista();
+        Lista<Autor> lista2 = new Lista();
+        Lista<Autor> lista3 = new Lista();
+        Lista<Autor> lista4 = new Lista();
+        
+        Autor a1 = new Autor("1");
+        Autor a2 = new Autor("2");
+        Autor a3 = new Autor("3");
+        Autor a4 = new Autor("4");
+        Autor a5 = new Autor("5");
+        Autor a6 = new Autor("6");
+        
+        lista1.insertar(new Nodo("1", a1));
+        lista1.insertar(new Nodo("2", a2));
+        lista1.insertar(new Nodo("3", a3));
+        lista1.insertar(new Nodo("4", a4));
+        
+        lista2.insertar(new Nodo("1", a1));
+        lista2.insertar(new Nodo("2", a2));
+        lista2.insertar(new Nodo("3", a3));
+        lista2.insertar(new Nodo("4", a4));
+        
+        lista3.insertar(new Nodo("1", a1));
+        lista3.insertar(new Nodo("5", a5));
+        lista3.insertar(new Nodo("3", a3));
+        lista3.insertar(new Nodo("4", a4));
+        
+        lista4.insertar(new Nodo("1", a1));
+        lista4.insertar(new Nodo("2", a2));
+        lista4.insertar(new Nodo("3", a3));
+        lista4.insertar(new Nodo("4", a4));
+        lista4.insertar(new Nodo("5", a5));
+        lista4.insertar(new Nodo("6", a6));
+        
+        System.out.println(lista1.equals(lista2));
+        System.out.println(lista1.equals(lista3));
+        System.out.println(lista1.equals(lista4));
     }
     
 }
