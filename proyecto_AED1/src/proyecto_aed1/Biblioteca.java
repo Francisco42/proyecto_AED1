3838580360d27cb0ce785470cc433a8af2526d5c
Unknown <franpiria42@gmail.com>
2018-05-15 16:27:57 -0300

    falta javadoc en cargador

--- proyecto_AED1/src/proyecto_aed1/Biblioteca.java
+++ proyecto_AED1/src/proyecto_aed1/Biblioteca.java
@@ -22,34 +22,67 @@ public class Biblioteca {
     }
 
     /**
+     * Retorna la lista de libros de la biblioteca.
      * 
-     * 
-     * @return 
+     * @return - La lista de libros.
      */
     public Lista<Libro> getListaLibros() {
         return listaLibros;
     }
 
+    /**
+     * Retorna la lista de tags de la biblioteca.
+     * 
+     * @return - La lista de tags.
+     */
     public Lista<Tag> getListaTags() {
         return listaTags;
     }
 
+    /**
+     * Retorna la lista de autores de la biblioteca.
+     * 
+     * @return - La lista de autores.
+     */
     public Lista<Autor> getListaAutores() {
         return listaAutores;
     }
     
+    /**
+     * Inserta un nuevo libro en la biblioteca.
+     * 
+     * @param id - El id del libro a insertar.
+     * @param libro - El libro a insertar.
+     */
     public void insertarLibro(int id, Libro libro) {
         listaLibros.insertar(new Nodo<>(id, libro));
     }
     
+    /**
+     * Inserta un nuevo autor en la biblioteca.
+     * 
+     * @param id - El id del autor a insertar.
+     * @param autor - El autor a insertar.
+     */
     public void insertarAutor(int id, Autor autor) {
         listaAutores.insertar(new Nodo<>(id, autor));
     }
     
+    /**
+     * Inserta un nuevo tag en la biblioteca.
+     * 
+     * @param id - El id del tag a insertar.
+     * @param tag - El tag a insertar.
+     */
     public void insertarTag(int id, Tag tag) {
         listaTags.insertar(new Nodo<>(id, tag));
     }
     
+    /**
+     * Muestra en pantalla todos los libros publicados a partir de cierto año.
+     * 
+     * @param año - El año de criterio.
+     */
     public void mostrarUltimasEdiciones(int año) {
         Nodo<Libro> aux = listaLibros.getPrimero();
         while (aux != null) {
@@ -60,6 +93,11 @@ public class Biblioteca {
         }
     }
     
+    /**
+     * Muestra en pantalla todos los libros que tienen un titulo dado.
+     * 
+     * @param titulo - El titulo que se busca.
+     */
     public void mostrarPorTitulo(String titulo) {
         Nodo<Libro> aux = listaLibros.getPrimero();
         while (aux != null) {
@@ -70,6 +108,12 @@ public class Biblioteca {
         }
     }
     
+    /**
+     * Muestra en pantalla todos los libros que fueron publicados en un 
+     * cierto año.
+     * 
+     * @param año - El año que se busca.
+     */
     public void mostrarPorAño(int año) {
         Nodo<Libro> aux = listaLibros.getPrimero();
         while (aux != null) {
@@ -80,6 +124,11 @@ public class Biblioteca {
         }
     }
     
+    /**
+     * Muestra en pantalla el libro que tiene el ISBN13 dado.
+     * 
+     * @param isbn - El ISBN13 que se busca.
+     */
     public void mostrarPorISBN(long isbn) {
         Nodo<Libro> aux = listaLibros.getPrimero();
         while (aux != null) {
@@ -91,6 +140,12 @@ public class Biblioteca {
         }
     }
     
+    /**
+     * Muestra en pantalla todos los libros que fueron escritos por un 
+     * cierto autor
+     * 
+     * @param autor - El autor que se busca.
+     */
     public void mostrarPorAutor(String autor) {
         Nodo<Autor> aux = listaAutores.getPrimero();
         while (aux != null) {
@@ -103,6 +158,11 @@ public class Biblioteca {
         }
     }
     
+    /**
+     * Muestra en pantalla todos los libros que llevan un cierto tag.
+     * 
+     * @param tag - El tag que se busca.
+     */
     public void mostrarPorTag(String tag) {
         Nodo<Tag> aux = listaTags.getPrimero();
         while (aux != null) {
@@ -115,6 +175,11 @@ public class Biblioteca {
         }
     }
     
+    /**
+     * Muestra toda la informacion de un libro, incluyendo sus autores y tags.
+     * 
+     * @param nombre - El nombre del libro que se busca.
+     */
     public void mostrarDetalles(String nombre) {
         Nodo<Libro> aux = listaLibros.getPrimero();
         while (aux != null) {
@@ -127,6 +192,11 @@ public class Biblioteca {
         }
     }
     
+    /**
+     * Elimina un autor de la biblioteca y todos los libros escritos por el.
+     * 
+     * @param autor - El autor a eliminar.
+     */
     public void eliminarAutor(String autor) {
         Nodo<Autor> auxAutor = listaAutores.buscar(autor);
         if (auxAutor != null) {
