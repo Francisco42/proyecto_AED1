083e6b77c39d9176dd304bf84887fc6c3b8e4b3c
Francisco Piria <franpiria42@gmail.com>
2018-05-20 15:03:46 -0300

    main

--- proyecto_AED1/src/proyecto_aed1/Proyecto_AED1.java
+++ proyecto_AED1/src/proyecto_aed1/Proyecto_AED1.java
@@ -5,6 +5,10 @@
  */
 package proyecto_aed1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author Francisco
@@ -14,45 +18,109 @@ public class Proyecto_AED1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here  
-//        Lista<Autor> lista1 = new Lista();
-//        Lista<Autor> lista2 = new Lista();
-//        Lista<Autor> lista3 = new Lista();
-//        Lista<Autor> lista4 = new Lista();
-//        
-//        Autor a1 = new Autor("1");
-//        Autor a2 = new Autor("2");
-//        Autor a3 = new Autor("3");
-//        Autor a4 = new Autor("4");
-//        Autor a5 = new Autor("5");
-//        Autor a6 = new Autor("6");
-//        
-//        lista1.insertar(new Nodo("1", a1));
-//        lista1.insertar(new Nodo("2", a2));
-//        lista1.insertar(new Nodo("3", a3));
-//        lista1.insertar(new Nodo("4", a4));
-//        
-//        lista2.insertar(new Nodo("1", a1));
-//        lista2.insertar(new Nodo("2", a2));
-//        lista2.insertar(new Nodo("3", a3));
-//        lista2.insertar(new Nodo("4", a4));
-//        
-//        lista3.insertar(new Nodo("1", a1));
-//        lista3.insertar(new Nodo("5", a5));
-//        lista3.insertar(new Nodo("3", a3));
-//        lista3.insertar(new Nodo("4", a4));
-//        
-//        lista4.insertar(new Nodo("1", a1));
-//        lista4.insertar(new Nodo("2", a2));
-//        lista4.insertar(new Nodo("3", a3));
-//        lista4.insertar(new Nodo("4", a4));
-//        lista4.insertar(new Nodo("5", a5));
-//        lista4.insertar(new Nodo("6", a6));
-//        
-//        System.out.println(lista1.equals(lista2));
-//        System.out.println(lista1.equals(lista3));
-//        System.out.println(lista1.equals(lista4));
+        Biblioteca biblioteca = new Biblioteca();
+        CargadorArchivosBiblioteca cargador = new CargadorArchivosBiblioteca();
+        
+        System.out.println("Cargando archivo \"Libros\" (1/5)...");
+        cargador.cargarLibros(biblioteca);
+        System.out.println("Archivo \"Libros\" cargado exitosamente!");
+        
+        System.out.println("Cargando archivo \"Autores\" (2/5)...");
+        cargador.cargarAutores(biblioteca);
+        System.out.println("Archivo \"Autores\" cargado exitosamente!");
+        
+        System.out.println("Cargando archivo \"Tags\" (3/5)...");
+        cargador.cargarTags(biblioteca);
+        System.out.println("Archivo \"Tags\" cargado exitosamente!");
+        
+        System.out.println("Cargando archivo \"LibrosAutores\" (4/5)...");
+        cargador.asociarLibrosAutores(biblioteca);
+        System.out.println("Archivo \"Libros\" cargado exitosamente!");
+        
+        System.out.println("Cargando archivo \"LibrosTags\" (5/5)...");
+        cargador.asociarLibrosTags(biblioteca);
+        System.out.println("Archivo \"LibrosTags\" cargado exitosamente!");
+        
+        String menu = "Menu Principal\n"
+                + "Elija una opcion y presione enter\n"
+                + "1 - Buscar un libro por titulo\n"
+                + "2 - Buscar libros por año\n"
+                + "3 - Buscar un libro por ISBN13\n"
+                + "4 - Buscar libros por autor\n"
+                + "5 - Buscar libros por tag\n"
+                + "6 - Buscar las ultimas ediciones a partir de una año\n"
+                + "7 - Mostrar los detalles de un libro en particular (a partir de su ISBN13)\n"
+                + "8 - Eliminar un autor de la biblioteca y todos sus libros asociados\n"
+                + "9 - Salir del programa\n";
+        
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        boolean salir = false;
+        while (!salir) {
+            System.out.println("");
+            System.out.println(menu);
+            String linea1 = buffer.readLine();
+            String linea2;
+            switch (linea1) {
+                
+                case "1":
+                    System.out.println("Ingrese el titulo a buscar");
+                    linea2 = buffer.readLine();
+                    biblioteca.mostrarPorTitulo(linea2);
+                    break;
+                    
+                case "2":
+                    System.out.println("Ingrese el año a buscar");
+                    linea2 = buffer.readLine();
+                    biblioteca.mostrarPorAño(Integer.parseInt(linea2));
+                    break;
+                    
+                case "3":
+                    System.out.println("Ingrese el ISBN13 a buscar");
+                    linea2 = buffer.readLine();
+                    biblioteca.mostrarPorISBN(Long.parseLong(linea2));
+                    break;
+                    
+                case "4":
+                    System.out.println("Ingrese el autor a buscar");
+                    linea2 = buffer.readLine();
+                    biblioteca.mostrarPorAutor(linea2);
+                    break;
+                    
+                case "5":
+                    System.out.println("Ingrese el tag a buscar");
+                    linea2 = buffer.readLine();
+                    biblioteca.mostrarPorTag(linea2);
+                    break;
+                    
+                case "6":
+                    System.out.println("Ingrese el año a buscar");
+                    linea2 = buffer.readLine();
+                    biblioteca.mostrarUltimasEdiciones(Integer.parseInt(linea2));
+                    break;
+                    
+                case "7":
+                    System.out.println("Ingrese el ISBN13 del libro a buscar");
+                    linea2 = buffer.readLine();
+                    biblioteca.mostrarDetalles(Long.parseLong(linea2));
+                    break;
+                    
+                case "8":
+                    System.out.println("Ingrese el autor a eliminar");
+                    linea2 = buffer.readLine();
+                    biblioteca.eliminarAutor(linea2);
+                    break;
+                    
+                case "9":
+                    salir = true;
+                    break;
+                    
+                default:
+                    System.out.println("La opcion ingresada no es valida");
+                    break;
+            }
+        }
     }
     
 }
