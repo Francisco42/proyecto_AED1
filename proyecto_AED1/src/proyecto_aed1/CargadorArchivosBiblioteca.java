be36c4c8794b98ae5d7e57d2c4e0c3707250b240
Francisco Piria <franpiria42@gmail.com>
2018-05-21 15:32:26 -0300

    ahora si

--- proyecto_AED1/src/proyecto_aed1/CargadorArchivosBiblioteca.java
+++ proyecto_AED1/src/proyecto_aed1/CargadorArchivosBiblioteca.java
@@ -5,7 +5,6 @@
  */
 package proyecto_aed1;
 
-import java.io.IOException;
 
 
 /**
@@ -22,27 +21,23 @@ public class CargadorArchivosBiblioteca {
      * @param biblioteca - La biblioteca a la cual se van a agregar libros.
      */
     public void cargarLibros(Biblioteca biblioteca) {
-        try {
-            String[] libros = ManejadorArchivosGenerico.leerArchivo("src/proyecto_aed1/books.csv");
-            for (String libro : libros) {
-                String[] partesLibro = libro.split("\\|");
-                
-                if ((partesLibro.length == 6) && (checkearVacios(partesLibro))) {
-                    try {
-                        int id = Integer.parseInt(partesLibro[0]);
-                        String titulo = partesLibro[1];
-                        short año = Short.parseShort(partesLibro[2]);
-                        float puntaje = Float.parseFloat(partesLibro[3]);
-                        int cantidadPuntajes = Integer.parseInt(partesLibro[4]);
-                        long isbn13 = Long.parseLong(partesLibro[5].split("\\/")[1]);
-                        
-                        Libro lib = new Libro(titulo, isbn13, año, puntaje, cantidadPuntajes);
-                        biblioteca.insertarLibro(id, lib);
-                    } catch (NumberFormatException ex) {}
-                }
+        String[] libros = ManejadorArchivosGenerico.leerArchivo("src/proyecto_aed1/books.csv");
+        for (String libro : libros) {
+            String[] partesLibro = libro.split("\\|");
+            
+            if ((partesLibro.length == 6) && (checkearVacios(partesLibro))) {
+                try {
+                    int id = Integer.parseInt(partesLibro[0]);
+                    String titulo = partesLibro[1];
+                    short año = Short.parseShort(partesLibro[2]);
+                    float puntaje = Float.parseFloat(partesLibro[3]);
+                    int cantidadPuntajes = Integer.parseInt(partesLibro[4]);
+                    long isbn13 = Long.parseLong(partesLibro[5].split("\\/")[1]);
+                    
+                    Libro lib = new Libro(titulo, isbn13, año, puntaje, cantidadPuntajes);
+                    biblioteca.insertarLibro(id, lib);
+                } catch (NumberFormatException ex) {}
             }
-        } catch (IOException ex) {
-            System.out.println("No se encontró el archivo a leer");
         }
     }
     
@@ -53,23 +48,19 @@ public class CargadorArchivosBiblioteca {
      * @param biblioteca - La biblioteca a la cual se van a agregar autores.
      */
     public void cargarAutores(Biblioteca biblioteca) {
-        try {
-            String[] autores = ManejadorArchivosGenerico.leerArchivo("src/proyecto_aed1/authors.csv");
-            for (String autor : autores) {
-                String[] partesAutor = autor.split("\\|");
-                
-                if ((partesAutor.length == 2) && (checkearVacios(partesAutor))) {
-                    try {
-                        int id = Integer.parseInt(partesAutor[0]);
-                        String nombre = partesAutor[1];
-                        
-                        Autor aut = new Autor(id, nombre);
-                        biblioteca.insertarAutor(id, aut);
-                    } catch (NumberFormatException ex) {}
-                }
+        String[] autores = ManejadorArchivosGenerico.leerArchivo("src/proyecto_aed1/authors.csv");
+        for (String autor : autores) {
+            String[] partesAutor = autor.split("\\|");
+            
+            if ((partesAutor.length == 2) && (checkearVacios(partesAutor))) {
+                try {
+                    int id = Integer.parseInt(partesAutor[0]);
+                    String nombre = partesAutor[1];
+                    
+                    Autor aut = new Autor(id, nombre);
+                    biblioteca.insertarAutor(id, aut);
+                } catch (NumberFormatException ex) {}
             }
-        } catch (IOException ex) {
-            System.out.println("No se encontró el archivo a leer");
         }
     }
     
@@ -80,23 +71,19 @@ public class CargadorArchivosBiblioteca {
      * @param biblioteca - La biblioteca a la cual se van a agregar tags.
      */
     public void cargarTags(Biblioteca biblioteca) {
-        try {
-            String[] tags = ManejadorArchivosGenerico.leerArchivo("src/proyecto_aed1/tags.csv");
-            for (String tag : tags) {
-                String[] partesTag = tag.split("\\|");
-                
-                if ((partesTag.length == 2) && (checkearVacios(partesTag))) {
-                    try {
-                        int id = Integer.parseInt(partesTag[0]);
-                        String nombre = partesTag[1];
-                        
-                        Tag tg = new Tag(nombre);
-                        biblioteca.insertarTag(id, tg);
-                    } catch (NumberFormatException ex) {}
-                }
+        String[] tags = ManejadorArchivosGenerico.leerArchivo("src/proyecto_aed1/tags.csv");
+        for (String tag : tags) {
+            String[] partesTag = tag.split("\\|");
+            
+            if ((partesTag.length == 2) && (checkearVacios(partesTag))) {
+                try {
+                    int id = Integer.parseInt(partesTag[0]);
+                    String nombre = partesTag[1];
+                    
+                    Tag tg = new Tag(nombre);
+                    biblioteca.insertarTag(id, tg);
+                } catch (NumberFormatException ex) {}
             }
-        } catch (IOException ex) {
-            System.out.println("No se encontró el archivo a leer");
         }
     }
     
@@ -109,31 +96,24 @@ public class CargadorArchivosBiblioteca {
      */
     public void asociarLibrosAutores(Biblioteca biblioteca) {
         
-        try {
-            Lista<Libro> listaLibros = biblioteca.getListaLibros();
+        Lista<Libro> listaLibros = biblioteca.getListaLibros();
+        Lista<Autor> listaAutores = biblioteca.getListaAutores();
+        String[] librosAutores = ManejadorArchivosGenerico.leerArchivo("src/proyecto_aed1/bookauthors.csv");
+        for (String libroAutor : librosAutores) {
+            String[] partes = libroAutor.split("\\|");
             
-            Lista<Autor> listaAutores = biblioteca.getListaAutores();
-            
-            String[] librosAutores = ManejadorArchivosGenerico.leerArchivo("src/proyecto_aed1/bookauthors.csv");
-            
-            for (String libroAutor : librosAutores) {
-                String[] partes = libroAutor.split("\\|");
-                
-                if ((partes.length == 2) && (checkearVacios(partes))) {
-                    try {
-                        int idLibro = Integer.parseInt(partes[0]);
-                        int idAutor = Integer.parseInt(partes[1]);
-                        
-                        Libro libro = listaLibros.buscar(idLibro).getDato();
-                        Autor autor = listaAutores.buscar(idAutor).getDato();
-                        
-                        libro.insertarAutor(idAutor, autor);
-                        autor.insertarLibro(idLibro, libro);
-                    } catch (NumberFormatException | NullPointerException ex) {}
-                }
+            if ((partes.length == 2) && (checkearVacios(partes))) {
+                try {
+                    int idLibro = Integer.parseInt(partes[0]);
+                    int idAutor = Integer.parseInt(partes[1]);
+                    
+                    Libro libro = listaLibros.buscar(idLibro).getDato();
+                    Autor autor = listaAutores.buscar(idAutor).getDato();
+                    
+                    libro.insertarAutor(idAutor, autor);
+                    autor.insertarLibro(idLibro, libro);
+                } catch (NumberFormatException | NullPointerException ex) {}
             }
-        } catch (IOException ex) {
-            System.out.println("No se encontró el archivo a leer");
         }
     }
     
@@ -146,31 +126,24 @@ public class CargadorArchivosBiblioteca {
      */
     public void asociarLibrosTags(Biblioteca biblioteca) {
         
-        try {
-            Lista<Libro> listaLibros = biblioteca.getListaLibros();
+        Lista<Libro> listaLibros = biblioteca.getListaLibros();
+        Lista<Tag> listaTags = biblioteca.getListaTags();
+        String[] librosTags = ManejadorArchivosGenerico.leerArchivo("src/proyecto_aed1/booktags.csv");
+        for (String libroTag : librosTags) {
+            String[] partes = libroTag.split("\\|");
             
-            Lista<Tag> listaTags = biblioteca.getListaTags();
-            
-            String[] librosTags = ManejadorArchivosGenerico.leerArchivo("src/proyecto_aed1/booktags.csv");
-            
-            for (String libroTag : librosTags) {
-                String[] partes = libroTag.split("\\|");
-                
-                if ((partes.length == 2) && (checkearVacios(partes))) {
-                    try {
-                        int idLibro = Integer.parseInt(partes[0]);
-                        int idTag = Integer.parseInt(partes[1]);
-                        
-                        Libro libro = listaLibros.buscar(idLibro).getDato();
-                        Tag tag = listaTags.buscar(idTag).getDato();
-                        
-                        libro.insertarTag(idTag, tag);
-                        tag.insertarLibro(idLibro, libro);
-                    } catch (NumberFormatException | NullPointerException ex) {}
-                }
+            if ((partes.length == 2) && (checkearVacios(partes))) {
+                try {
+                    int idLibro = Integer.parseInt(partes[0]);
+                    int idTag = Integer.parseInt(partes[1]);
+                    
+                    Libro libro = listaLibros.buscar(idLibro).getDato();
+                    Tag tag = listaTags.buscar(idTag).getDato();
+                    
+                    libro.insertarTag(idTag, tag);
+                    tag.insertarLibro(idLibro, libro);
+                } catch (NumberFormatException | NullPointerException ex) {}
             }
-        } catch (IOException ex) {
-            System.out.println("No se encontró el archivo a leer");
         }
     }
     
